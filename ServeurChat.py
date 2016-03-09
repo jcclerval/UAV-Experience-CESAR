4dccb06a891ea78f813c52171cfda6a1769d0cba
Jean-Christophe <jcclerval@aol.com>
2016-03-09 13:03:31 +0100

    Modif

--- ServeurChat.py
+++ ServeurChat.py
@@ -6,7 +6,7 @@ Created on Wed Mar  9 10:51:22 2016
 """
 import socket
 Sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-socket.bind(('', 234))
+Sock.bind(('', 15555))
 # On est a l'ecoute d'une seule et unique connexion :
 Sock.listen(1)
  
@@ -14,7 +14,14 @@ Sock.listen(1)
 client, adresse = Sock.accept() # accepte les connexions de l'exterieur
 print "L'adresse",adresse,"vient de se connecter au serveur !"
 while 1:
-        RequeteDuClient = client.recv(255) # on recoit 255 caracteres grand max
-        if not RequeteDuClient: # si on ne recoit plus rien
-                break  # on break la boucle (sinon les bips vont se repeter)
-        print RequeteDuClient,"\a"         # affiche les donnees envoyees, suivi d'un bip sonore
+    RequeteDuClient = client.recv(255) # on recoit 255 caracteres grand max
+    if not RequeteDuClient: # si on ne recoit plus rien
+        break  # on break la boucle (sinon les bips vont se repeter)
+    print RequeteDuClient,"\a"         # affiche les donnees envoyees, suivi d'un bip sonore
+
+    if (RequeteDuClient =='quit'):
+        break
+    
+client.close()
+Sock.close()
+print('Fin de la connexion')
--- SocketRasp.py
+++ SocketRasp.py
@@ -13,7 +13,7 @@ Sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # on cree notre socket
  
 # definition des informations :
 Host = 'bord3l'
-Port = 234
+Port = 15555
  
 # on se connecte sur le serveur avec les informations ci-dessus
 # assurez-vous d'avoir mis en marche le serveur !
@@ -21,7 +21,10 @@ Sock.connect((Host,Port))
  
 # On est connecte, on fait une boucle infinie d'inputs pour l'envoi des messages :
 while 1:
-        msg = raw_input('>> ')  # on rentre des donnees
+        msg = raw_input('(quit pour fermer) >> ')  # on rentre des donnees
         Sock.send(msg) # on envoie ces donnees
+        if (msg =='quit'):
+            print("Fermeture de la connexion")
+            break
  
 # regardez ce qui se passe du cote serveur.
