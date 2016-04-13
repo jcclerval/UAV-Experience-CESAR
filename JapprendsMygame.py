62aafbe8b89f49ac6cc149b0e8986b941f2e16a7
Jean-Christophe <jcclerval@aol.com>
2016-04-13 09:29:06 +0200

    Mise à jour de l'adresse IP pour connexion ad-hoc

--- JapprendsMygame.py
+++ JapprendsMygame.py
@@ -13,31 +13,32 @@ import socket                                                                  #
 Sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)                        # on cree notre socket
 
  
-# definition des informations -------------------------------------------------
-Host = '10.16.155.25'
+# Definition des informations -------------------------------------------------
+# Host = '192.168.43.151'
+Host = '10.42.0.14'
 Port = 15550
 
-# on se connecte sur le serveur avec les informations ci-dessus
-# assurez-vous d'avoir mis en marche le serveur !
+# On se connecte sur le serveur avec les informations ci-dessus
+# Assurez-vous d'avoir mis en marche le serveur !
 Sock.connect((Host,Port))
 
 # Lancemement de PyGame -------------------------------------------------------
 pygame.init()
 
 
-#Ouverture de la fenêtre Pygame -----------------------------------------------
+# Ouverture de la fenêtre Pygame ----------------------------------------------
 
 fenetre = pygame.display.set_mode((640, 480))
 
 
-#Chargement et collage du fond ------------------------------------------------
+# Chargement et collage du fond -----------------------------------------------
 
 fond = pygame.image.load("background.jpg").convert()
 
 fenetre.blit(fond, (0,0))
 
 
-#Chargement et collage du personnage ------------------------------------------
+# Chargement et collage du personnage -----------------------------------------
 
 perso = pygame.image.load("perso.png").convert_alpha()
 
@@ -46,54 +47,93 @@ position_perso = perso.get_rect()
 fenetre.blit(perso, position_perso)
 
 
-#Rafraîchissement de l'écran --------------------------------------------------
+# Rafraîchissement de l'écran -------------------------------------------------
 
 pygame.display.flip()
 
 
-#BOUCLE INFINIE ---------------------------------------------------------------
+# On compte les joysticks -----------------------------------------------------
+
+nb_joysticks = pygame.joystick.get_count()
+
+# BOUCLE INFINIE --------------------------------------------------------------
 
 continuer = 1
-pygame.key.set_repeat(400, 30)
+pygame.key.set_repeat(10, 50)
 
-while continuer:
 
-    for event in pygame.event.get():    #Attente des événements
+if nb_joysticks > 0:
+    mon_joystick = pygame.joystick.Joystick(0)
 
-        if event.type == QUIT:
-            print("Fermeture de la connexion")
-            continuer = 0
 
-        if event.type == KEYDOWN:
-
-            if event.key == K_DOWN:                                            # Si "flèche bas"
-                position_perso = position_perso.move(0,3)
-                Sock.send('arriere')                                           # On envoie ces donnees
-            
-            if event.key == K_UP:                                              # Si "flèche haut"
-                position_perso = position_perso.move(0,-3)
-                Sock.send('avant')                                             # On envoie ces donnees
+    mon_joystick.init()
+    
+    while continuer:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                print("Fermeture de la connexion")
+                continuer = 0
                 
-            if event.key == K_RIGHT:                                           # Si "flèche droite"
-                position_perso = position_perso.move(3,0)
-                Sock.send('droit')                                             # On envoie ces donnees
+            x = mon_joystick.get_axis(0);
+            x = int(x*100)
+            y = mon_joystick.get_axis(1);
+            y = int(y*100)
+                       
+            data = (x,y)
+            Sock.send(data)
+            position_perso = position_perso.move(int(x/10),int(y/10)
+        # Re-collage ----------------------------------------------------------
+
+        fenetre.blit(fond, (0,0))   
+
+        fenetre.blit(perso, position_perso)
+
+        # Rafraichissement ----------------------------------------------------
+
+        pygame.display.flip()
+
+    while continuer:
+
+        for event in pygame.event.get():                                       # Attente des événements
+        
+            if event.type == QUIT:
+                print("Fermeture de la connexion")
+                continuer = 0
+
+            if event.type == KEYDOWN:
+                
+                if event.key == K_DOWN:                                        # Si "flèche bas"
+                    position_perso = position_perso.move(0,3)
+                    Sock.send('arriere')                                       # On envoie ces donnees
             
-            if event.key == K_LEFT:                                            # Si "flèche gauche"
-                position_perso = position_perso.move(-3,0)
-                Sock.send('gauche')                                            # On envoie ces donnees
+                if event.key == K_UP:                                          # Si "flèche haut"
+                    position_perso = position_perso.move(0,-3)
+                    Sock.send('avant')                                         # On envoie ces donnees
+                
+                if event.key == K_RIGHT:                                       # Si "flèche droite"
+                    position_perso = position_perso.move(3,0)
+                    Sock.send('droit')                                         # On envoie ces donnees
+            
+                if event.key == K_LEFT:                                        # Si "flèche gauche"
+                    position_perso = position_perso.move(-3,0)
+                    Sock.send('gauche')                                        # On envoie ces donnees
+
+                if event.key == K_SPACE:                                       # Si "flèche gauche"
+                    position_perso = position_perso.move(-3,0)
+                    Sock.send('stop')                                          # On envoie ces donnees
 
 
     
 
-    #Re-collage ---------------------------------------------------------------
+        # Re-collage ----------------------------------------------------------
 
-    fenetre.blit(fond, (0,0))   
+        fenetre.blit(fond, (0,0))   
 
-    fenetre.blit(perso, position_perso)
+        fenetre.blit(perso, position_perso)
 
-    #Rafraichissement ---------------------------------------------------------
+        # Rafraichissement ----------------------------------------------------
 
-    pygame.display.flip()
+        pygame.display.flip()
     
 print("Fin du script")
 
